4e1dd28c7aebf8ac81b9ce8c692d963f689489de
Pawid-prianto27 <pawitanto27@gmail.com>
2021-05-17 18:51:45 +0700

    how to deploy on netlify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const add = () => {
+    setCount(count + 1);
+  }
+
+  const minus = () => {
+    if(count == 0) {
+      alert('Opps.. counter tidak boleh kurang dari 0')
+    } else {
+      setCount(count - 1)
+    }
+    
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {count}
+        <div className="wraper">
+        <button onClick={add} style={{backgroundColor: 'green', borderRadius: 20, height: 40, width: 100}}>
+          Tambah
+        </button>
+        <button onClick={minus} style={{backgroundColor: 'green', borderRadius: 20, height: 40, width: 100}}>
+          Kurang
+        </button>
+        </div>
       </header>
+      
     </div>
   );
 }
